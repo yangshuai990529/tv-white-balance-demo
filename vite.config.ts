efdb7122f4a55b1a9d679b6a590f8b95eee7a93d
Eric_AI_PM <yangshuai990529@gmail.com>
2026-07-04 02:08:53 +0800

    Update base for new repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tv-white-balance-calibration/',
+  base: '/tv-white-balance-demo/',
   plugins: [react()],
 })
